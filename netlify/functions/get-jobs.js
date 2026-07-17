4a6e61204e3d39227c5872135840bf8407a4c0f1
dedinguyen <dedi@clccontractors.com>
2026-07-17 10:24:49 -0400

    Implement pagination for job queries
    Refactor job retrieval to support pagination and limit results to 100 per request.

--- netlify/functions/get-jobs.js
+++ netlify/functions/get-jobs.js
@@ -2,24 +2,40 @@ const { jobtreadQuery, orgId } = require("./_jobtread");
 
 exports.handler = async () => {
   try {
-    const data = await jobtreadQuery({
-      organization: {
-        $: { id: orgId() },
-        jobs: {
-          $: {
-            size: 200,
-            where: ["closedOn", null],
-            sortBy: [{ field: "name", order: "asc" }],
-          },
-          nodes: { id: {}, name: {}, number: {} },
-        },
-      },
-    });
+    let jobs = [];
+    let page = undefined;
+    let safety = 0;
 
-    const jobs = data.organization.jobs.nodes.map((j) => ({
-      id: j.id,
-      name: j.number ? `${j.name} (#${j.number})` : j.name,
-    }));
+    while (safety < 10) {
+      safety++;
+      const data = await jobtreadQuery({
+        organization: {
+          $: { id: orgId() },
+          jobs: {
+            $: {
+              size: 100,
+              page,
+              where: ["closedOn", null],
+              sortBy: [{ field: "name", order: "asc" }],
+            },
+            nodes: { id: {}, name: {}, number: {} },
+            nextPage: {},
+          },
+        },
+      });
+
+      const nodes = data.organization.jobs.nodes || [];
+      jobs = jobs.concat(
+        nodes.map((j) => ({
+          id: j.id,
+          name: j.number ? `${j.name} (#${j.number})` : j.name,
+        }))
+      );
+
+      const next = data.organization.jobs.nextPage;
+      if (!next) break;
+      page = next;
+    }
 
     return {
       statusCode: 200,
